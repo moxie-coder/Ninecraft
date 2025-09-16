f276d1aebcd2850653de3d4b8a960853973817a2
MFDGaming <alexandros.argentakis@gmail.com>
2025-09-16 03:06:02 +0300

    made the usernames mod work for 0.5.0-0.7.1

--- ninecraft/src/mods/usernames_mod.c
+++ ninecraft/src/mods/usernames_mod.c
@@ -12,18 +12,11 @@ void usernames_mod_inject(int version_id) {
     if (!player_renderer_render_name) {
         return;
     }
-    if (version_id == version_id_0_6_1 || version_id == version_id_0_6_0) {
+    if (version_id >= version_id_0_5_0 && version_id <= version_id_0_7_1) {
 #if defined(__i386__) || defined(_M_IX86)
         patch_address((char *)player_renderer_render_name + 50, "\x90\x90", 2, PATCH_ADDRESS_PROT_XR);
 #endif
 #if defined(__arm__) || defined(_M_ARM)
-        patch_address((char *)player_renderer_render_name + 23, "\x00\xbf", 2, PATCH_ADDRESS_PROT_XR);
-#endif
-    } else if (version_id == version_id_0_5_0) {
-#if defined(__i386__) || defined(_M_IX86)
-        patch_address((char *)player_renderer_render_name + 50, "\x90\x90", 2, PATCH_ADDRESS_PROT_XR);
-#endif
-#if defined(__arm__) || defined(_M_ARM) 
         patch_address((char *)player_renderer_render_name + 23, "\x00\xbf", 2, PATCH_ADDRESS_PROT_XR);
 #endif
     }
